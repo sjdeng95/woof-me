11f6d2d7da996fed2c3afccf5f196508b74e2af2
sloan-n <sloann@oregonstate.edu>
2023-07-13 13:46:39 -1000

    Update public swipe screen UI

--- lib/screens/public_screens/swipe_screen.dart
+++ lib/screens/public_screens/swipe_screen.dart
@@ -4,15 +4,12 @@ class SwipeScreen extends StatefulWidget {
   const SwipeScreen({super.key});
 
   @override
-  State<SwipeScreen> createState() =>
-      _SwipeScreenState();
+  State<SwipeScreen> createState() => _SwipeScreenState();
 }
 
-class _SwipeScreenState
-    extends State<SwipeScreen> {
+class _SwipeScreenState extends State<SwipeScreen> {
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-
 
   @override
   Widget build(BuildContext context) {
@@ -20,12 +17,51 @@ class _SwipeScreenState
       appBar: AppBar(
         title: const Text('Find Your Pawfect Match'),
       ),
-      body: const Center(
-        child: Text(
-          'SWIPE SCREEN',
-          style: optionStyle,
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 500,
+                child: Placeholder(),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'PET NAME - AGE',
+                style: optionStyle,
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Type - Breed',
+                style: optionStyle,
+              ),
+              const SizedBox(height: 20),
+              ButtonBar(
+                  alignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back_ios_new_rounded,
+                          size: 50.0),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.cancel, size: 50.0),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.favorite_rounded, size: 50.0),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.arrow_forward_ios_rounded,
+                          size: 50.0),
+                      onPressed: () {},
+                    ),
+                  ])
+            ]),
       ),
-    ); 
+    );
   }
 }
