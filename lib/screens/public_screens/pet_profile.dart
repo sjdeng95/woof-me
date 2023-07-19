89da589d124bd28846472f7a06468a04d3958ff7
sloan-n <sloann@oregonstate.edu>
2023-07-18 23:29:40 -1000

    Update screen to get pet info from database

--- lib/screens/public_screens/pet_profile.dart
+++ lib/screens/public_screens/pet_profile.dart
@@ -13,6 +13,52 @@ class _PetProfileScreenState extends State<PetProfileScreen> {
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
 
+  Widget aboutPet(PetInfo petInfo) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 400,
+            child: Placeholder(),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            '${petInfo.name}',
+            style: optionStyle,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            '${petInfo.availability}',
+            style: optionStyle,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            '${petInfo.type} - ${petInfo.breed}',
+            style: optionStyle,
+          ),
+          const SizedBox(height: 20),
+          CheckboxListTile(
+            title: const Text("Good with other animals"),
+            value: true,
+            onChanged: (value) {},
+          ),
+          CheckboxListTile(
+            title: const Text("Good with children"),
+            value: true,
+            onChanged: (value) {},
+          ),
+          CheckboxListTile(
+            title: const Text("Must be on leash at all times"),
+            value: true,
+            onChanged: (value) {},
+          ),
+          const SizedBox(height: 40),
+          SizedBox(height: 40, child: Text('${petInfo.story}')),
+          const SizedBox(height: 40),
+        ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     // final PetInfo petInfo =
