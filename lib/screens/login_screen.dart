b0fd4066ddd741b4ffd4954af782b95e0095022d
sloan-n <sloann@oregonstate.edu>
2023-07-23 21:03:38 -1000

    Created login screen with firebase authentication for email/password

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:woofme/widgets/login_form.dart';
+import 'package:woofme/widgets/public_navigation.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -11,78 +14,16 @@ class _LoginScreenState extends State<LoginScreen> {
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
 
-  final _formKey = GlobalKey<FormState>();
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Woof-Me'),
-      ),
-      body: loginForm(),
-    );
-  }
-
-  Form loginForm() {
-    return Form(
-        key: _formKey,
-        child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: "Email"),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your email';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 10.0),
-                  TextFormField(
-                    controller: passwordController,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: "Password"),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your password';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 15.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        // Navigate the user to the Home page
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Please fill input')),
-                        );
-                      }
-                    },
-                    child: const Text('Log-in'),
-                  ),
-                  const SizedBox(height: 10.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Navigate to sign-up screen
-                    },
-                    child: const Text('Sign-up'),
-                  ),
-                  const SizedBox(height: 20.0),
-                  GestureDetector(
-                      child: const Text('Forget Username?'), onTap: () {}),
-                  const SizedBox(height: 10.0),
-                  GestureDetector(
-                      child: const Text('Forget Password?'), onTap: () {}),
-                ])));
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const PublicNavigation();
+          } else {
+            return const LoginForm();
+          }
+        });
   }
 }
